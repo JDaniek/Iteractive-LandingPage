8e6d581270c015b33f9e669c09e4c7a2b3e01494
JDaniek <danielorantes270@gmail.com>
2026-07-16 14:28:50 -0600

    feat: Logica para desplegar submenu aplicada

--- JS/menu.js
+++ JS/menu.js
@@ -3,5 +3,22 @@ const navOverlay = document.querySelector(".nav__screen");
 
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("nav__hamburguer--open");
-  navOverlay.classList.toggle("nav__screen--show ");
+  navOverlay.classList.toggle("nav__screen--show");
 });
+
+navOverlay.addEventListener("click", (e) => {
+  e.preventDefault();
+  const currentElement = e.target;
+  if (isActive(currentElement, "nav__parent")) {
+    const subMenu = currentElement.parentElement.children[1];
+    if (window.innerWidth < 768) {
+      let height = subMenu.clientHeight == 0 ? subMenu.scrollHeight : 0;
+      subMenu.style.height = `${height}px`;
+    } else {
+    }
+  }
+});
+
+function isActive(element, string) {
+  return element.classList.value.includes(string);
+}
