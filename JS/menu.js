ac6bec3999bceb8a691ade892c8a7a39d32738dc
JDaniek <danielorantes270@gmail.com>
2026-07-15 21:48:37 -0600

    feat: Funcionalidad de espliegue y cierre de menu implementada

--- JS/menu.js
+++ JS/menu.js
@@ -1,5 +1,7 @@
 const menuIcon = document.querySelector(".nav__hamburguer");
+const navOverlay = document.querySelector('.nav__screen')
 
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("nav__hamburguer--open");
+  navOverlay.classList.toggle('nav__screen--show ')
 });
