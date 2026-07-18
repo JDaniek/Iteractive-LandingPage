fd5e1612e5260368b3152121621d1720c178bd6e
JDaniek <danielorantes270@gmail.com>
2026-07-17 20:57:21 -0600

    feat: submenu desactivado para la version desktop

--- JS/menu.js
+++ JS/menu.js
@@ -1,11 +1,13 @@
 const menuIcon = document.querySelector(".nav__hamburguer");
 const navOverlay = document.querySelector(".nav__screen");
 
+// Mostrar el menu u coultarlo
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("nav__hamburguer--open");
   navOverlay.classList.toggle("nav__screen--show");
 });
 
+// Desplegar sub menus
 navOverlay.addEventListener("click", (e) => {
   const currentElement = e.target;
   if (isActive(currentElement, "nav__parent")) {
@@ -15,6 +17,17 @@ navOverlay.addEventListener("click", (e) => {
       subMenu.style.height = `${height}px`;
     } else {
     }
+  }
+});
+
+// desactivar sub menus
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    const navInners = document.querySelectorAll(".nav__inner");
+    navInners.forEach((navInner) => {
+      navInner.style.height = "";
+    });
   }
 });
 
